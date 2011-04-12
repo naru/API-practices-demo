2c0c858a833aa64d7d3f5e18344ba156fea6010b
Anton Narusberg <antonnarusberg@mackie.lan>
2011-04-12 19:50:17 +0300

    initial commit

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.6'
+
+gem 'mysql2'
+
+group :test, :development do
+  gem 'rspec-rails', '>= 2.0.0.beta.22'
+  gem 'factory_girl_rails', '1.0'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Appi::Application.config.secret_token = 'a6ddb2a4e960e2ef61e091cf26ceeac7957ed17615e7bdff99a667f512d6c6b3dab8f80dbfa5661aaaf1fe0f677233ca18c5301a56a61051a4fbb35ac2b31952'
