f2d8dc7c71e754d57c825ff54c8265b52222422c
Anton Narusberg <antonnarusberg@Mackie.local>
2011-04-14 18:03:56 +0300

    no need for ensure json

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -4,22 +4,12 @@ class API::ApplicationController < ApplicationController
 
   respond_to :json
 
-  before_filter :ensure_json
-
-  private
-
   def parse_body_json
     @attributes = JSON.parse(request.body.read)
   end
 
   def record_not_found
     head :not_found
-  end
-
-  def ensure_json
-    unless request.format.json?
-      head :not_acceptable
-    end
   end
 
 end
